f740d530cd739fa24b136fc01c904e5a94a3ce25
Danny Jackson <dnjacks4@asu.edu>
2025-01-24 08:36:56 -0700

    Update fst.sh

--- fst.sh
+++ fst.sh
@@ -5,7 +5,7 @@
 # all parameters come from the fst_param control file
 # make edits there before using this script!
 
-scriptdir=~/programs/msmc2_scripts
+scriptdir=~/programs/Intro_Bioinformatics_Workshop
 source ${scriptdir}/fst_params.sh
 
 printf "\n \n \n \n"
